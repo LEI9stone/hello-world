3aff7568e3da622d5a719bd73a2da6b3df3974c6
无始 <wushi@rouzao.com>
2026-09-10 10:31:49 +0800

    refactor: remove AntdProvider component as it's no longer needed

--- day-day-up-up/src/components/AntdProvider.tsx
+++
@@ -1,8 +0,0 @@
-import { ConfigProvider } from 'antd';
-import zhCN from 'antd/locale/zh_CN';
-import 'dayjs/locale/zh-cn';
-import type { PropsWithChildren } from 'react';
-
-export default function AntdProvider({ children }: PropsWithChildren) {
-  return <ConfigProvider locale={zhCN}>{children}</ConfigProvider>;
-}
